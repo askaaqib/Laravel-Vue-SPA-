8ccf08edc5999f9267fad0451369233b5409534a
Aqib <hafizaqibakhtar@nitb.gov.pk>
2025-02-16 22:54:36 +0500

    changes

--- frontend/vuejs/frontend/src/views/auth/Register.vue
+++ frontend/vuejs/frontend/src/views/auth/Register.vue
@@ -1,4 +1,51 @@
-<script setup lang="en"></script>
+<script setup lang="ts">
+import axiosInstance from "@/lib/axios.ts";
+
+interface 
+const register = async () => {
+    await axiosInstance.get("/sanctum/csrf-cookie", {
+        baseURL: "http://localhost:8000"
+    });
+
+    try {
+        const response = await axiosInstance.post(
+            "register",
+            {
+                name: 'John Doe',
+                email: 'johndoe@mail.com',
+                password: 'password',
+                password_confirmation: 'password',
+            }
+        );
+    } catch (error)
+    {
+        console.log(error);
+    }
+}
+
+register();
+</script>
 <template>
-    <h1>Register</h1>
+    <h1 class="text-3xl p-4 mx-auto">Register</h1>
+
+    <form class="max-w-sm mx-auto p-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div class="mb-5">
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+            <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your Name" required />
+        </div>
+        <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+        </div>
+        <div class="mb-5">
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+            <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+        </div>
+        <div class="mb-5">
+            <label for="password_confirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+            <input type="password_confirmation" id="password_confirmation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+        </div>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </form>
+
 </template>
